05988397cb0c9a876a654a0113ed5620c11f7260
serverless-admin-at-880992168432 <mayank.chugh77@gmail.com>
2020-08-30 15:39:44 +0800

    After custom build burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,24 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    // constructor(props){
+    //     super(prop);
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
     render() {
         return (
             <Auxiliary>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div> Build Controls </div>
             </Auxiliary>
 
